d2681d660636928a1c4d3ccf5edead88dd9d6ff5
andtr-2021 <s3878698@rmit.edu.vn>
2023-07-26 14:21:23 +0700

    add hx711 reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
+#include <HX711.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int LOADCELL_DOUT_PIN = 16; // Change this to your ESP32 GPIO pin for HX711 data (DT)
+const int LOADCELL_SCK_PIN = 4;  // Change this to your ESP32 GPIO pin for HX711 clock (SCK)
+
+HX711 scale;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("HX711 Weight Measurement");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(350);   // Set this to the calibration factor of your load cell (read below for calibration)
+  scale.tare();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float weight = scale.get_units(); // Get weight in units (adjust with calibration factor)
+  Serial.print("Weight: ");
+  Serial.print(weight);
+  Serial.println(" grams");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000); // Wait for a second before reading again
 }
