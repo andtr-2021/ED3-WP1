98961da6bd5033092e310254e92383ea64dbe1bd
andtr-2021 <s3878698@rmit.edu.vn>
2023-08-02 16:33:20 +0700

    modify a bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,39 @@
 #include <Arduino.h>
 #include <HX711_ADC.h>
 
-// ******************************************
-// General information
-// ******************************************
-// HX711 by default has a data rate of 10Hz. It is possible to change it to 80Hz, but requires some tinkering on the board
-// Change the config.h file in .pio/libdeps/HX711_ADC/src/config.h to have smoother or faster data
-
-// ******************************************
-// Variable declaration
-// ******************************************
-// Loadcell parameters
 #define LOADCELL_DT_PIN 16
 #define LOADCELL_SCK_PIN 4
+
 HX711_ADC scale(LOADCELL_DT_PIN, LOADCELL_SCK_PIN);
+
 #define calibration_factor 460 // Depends on the load cell
+
 volatile boolean newDataReady;
+
 float loadcell_data; // measurement from the loadcell in gr
-// Serial configuration parameters
+
+// serial configuration parameters
 unsigned long curr_time = 0, prev_time = 0, dt = 50000; // time interval in us
 
-// ******************************************
-// Function declaration
-// ******************************************
+// functions declarations
 void dataReadyISR();
 void SerialDataWrite();
 
-// ******************************************
-// void setup
-// ******************************************
 void setup()
 {
   Serial.begin(9600);
-  while (!Serial)
-    ;
+  while (!Serial);
   Serial.println("Loadcell calibration, please wait.");
   scale.begin();
   scale.start(2000, true);
   scale.setCalFactor(calibration_factor);
   Serial.println("Loadcell calibrated. Press any key to start the main program.");
-  while (!Serial.available())
-    ;
+  while (!Serial.available());
   Serial.read();
   pinMode(LOADCELL_DT_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(LOADCELL_DT_PIN), dataReadyISR, FALLING);
 }
 
-// ******************************************
-// void loop
-// ******************************************
 void loop()
 {
   if (newDataReady)
@@ -64,9 +49,9 @@ void loop()
     SerialDataWrite();
   }
 }
-// ******************************************
-// Function definitions
-// ******************************************
+
+// fuctions definitions
+
 void dataReadyISR()
 {
   if (scale.update())
