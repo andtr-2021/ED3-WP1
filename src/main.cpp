d94dd124d551b23c34c590d417243fa20a72c185
andtr-2021 <s3878698@rmit.edu.vn>
2023-08-02 15:12:29 +0700

    add the loadcell calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,82 @@
 #include <Arduino.h>
-#include <HX711.h>
+#include <HX711_ADC.h>
 
-const int LOADCELL_DOUT_PIN = 16; // Change this to your ESP32 GPIO pin for HX711 data (DT)
-const int LOADCELL_SCK_PIN = 4;  // Change this to your ESP32 GPIO pin for HX711 clock (SCK)
+// ******************************************
+// General information
+// ******************************************
+// HX711 by default has a data rate of 10Hz. It is possible to change it to 80Hz, but requires some tinkering on the board
+// Change the config.h file in .pio/libdeps/HX711_ADC/src/config.h to have smoother or faster data
 
-HX711 scale;
+// ******************************************
+// Variable declaration
+// ******************************************
+// Loadcell parameters
+#define LOADCELL_DT_PIN 16
+#define LOADCELL_SCK_PIN 4
+HX711_ADC scale(LOADCELL_DT_PIN, LOADCELL_SCK_PIN);
+#define calibration_factor 460 // Depends on the load cell
+volatile boolean newDataReady;
+float loadcell_data; // measurement from the loadcell in gr
+// Serial configuration parameters
+unsigned long curr_time = 0, prev_time = 0, dt = 50000; // time interval in us
 
-void setup() {
+// ******************************************
+// Function declaration
+// ******************************************
+void dataReadyISR();
+void SerialDataWrite();
+
+// ******************************************
+// void setup
+// ******************************************
+void setup()
+{
   Serial.begin(9600);
-  Serial.println("HX711 Weight Measurement");
-
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(350);   // Set this to the calibration factor of your load cell (read below for calibration)
-  scale.tare();
+  while (!Serial)
+    ;
+  Serial.println("Loadcell calibration, please wait.");
+  scale.begin();
+  scale.start(2000, true);
+  scale.setCalFactor(calibration_factor);
+  Serial.println("Loadcell calibrated. Press any key to start the main program.");
+  while (!Serial.available())
+    ;
+  Serial.read();
+  pinMode(LOADCELL_DT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(LOADCELL_DT_PIN), dataReadyISR, FALLING);
 }
 
-void loop() {
-  float weight = scale.get_units(); // Get weight in units (adjust with calibration factor)
-  Serial.print("Weight: ");
-  Serial.print(weight);
-  Serial.println(" grams");
+// ******************************************
+// void loop
+// ******************************************
+void loop()
+{
+  if (newDataReady)
+  {
+    newDataReady = 0;
+    loadcell_data = scale.getData();
+  }
 
-  delay(1000); // Wait for a second before reading again
+  curr_time = micros();
+  if (curr_time - prev_time >= dt)
+  {
+    prev_time += dt;
+    SerialDataWrite();
+  }
+}
+// ******************************************
+// Function definitions
+// ******************************************
+void dataReadyISR()
+{
+  if (scale.update())
+    newDataReady = 1;
+}
+
+void SerialDataWrite()
+{
+  Serial.print(curr_time / 1000);
+  Serial.print(",");
+  Serial.print(loadcell_data);
+  Serial.println("");
 }
